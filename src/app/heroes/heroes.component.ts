31f13d848f6a1a521c73e0bcdd626992d67c0207
Kyle Middleton <kylemid321@gmail.com>
2019-05-09 16:53:25 -0600

    add add method

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -25,4 +25,13 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes); //observable is used here for asynchronous operation
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero)
+      })
+  }
 }
